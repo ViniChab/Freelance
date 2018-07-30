4d4d6357b370f2c6c0a8caf43a3106440fffb6f8
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-07-30 15:46:12 -0300

    Create  JavaFactorial(Method2).java

---
+++ JavaFactorial(Method2).java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+package fatorial;
+public class Fatorial 
+{
+    public static int fat ( int x )
+    {
+        if( x == 0 || x == 1)
+          return 1;
+        
+        else
+        
+          return x*fat(x-1);
+    }
+    public static void main(String[] args) 
+    {
+        int num;
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Please, type in a number: ");
+        num = scan.nextInt();
+        System.out.println("\nO fatoral deste numero é: "+fat(num));
+    }
+    
+}
