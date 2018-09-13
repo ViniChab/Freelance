4fe232fb2c57ed953d41d9daa5895b057eca1769
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-13 13:18:20 -0300

    Create button-click.js

---
+++ button-click.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	
+	<style>
+	iframe
+	{
+		width: 100%;
+		height: 635px;
+		margin: 0;
+		border: 0;
+		padding: 0;
+	}
+	
+	button
+	{
+		position: absolute;
+		top: 300px;
+	}
+	
+	</style>
+	
+	<script>
+		
+		function likeFunction() 
+			{
+			var iframe = document.getElementById("myFrame");
+			var elmnt = iframe.contentWindow.document.getElementsByTagName("a")[0];
+			elmnt.click();
+		}
+		
+	</script>
+	
+	<iframe id='myFrame' src=https://billwurtz.com/>
+	</iframe>
+	
+	<button onclick="likeFunction()"> Click me! </button>
+	
+</html>
