48bdb4530ecb6b7277019ba0bf1eba3d8a08b104
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-18 20:40:39 -0300

    Create JavaMath.java

---
+++ Java/JavaMath.java
@@ -0,0 +1,16 @@
+
+public class test {
+	
+	public static void main(String[] args) 
+	{
+		
+		int root = 16;
+		double b;
+		b = Math.sqrt(root);
+		System.out.println(b);
+		
+		b = Math.pow(root, 2);
+		System.out.println(b); /*assim que funciona a biblioteca de matemática do Java*/
+		
+	}
+}
