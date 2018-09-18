b8f7132febb044041f282d9e1f9c9ae0985d30fb
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-18 20:50:47 -0300

    Create UsingScanner.java

---
+++ Java/UsingScanner.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class test {
+	
+	private static Scanner scan;
+
+	public static void main(String[] args) 
+	{
+	
+		scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		System.out.println(a);
+		
+	}
+}
