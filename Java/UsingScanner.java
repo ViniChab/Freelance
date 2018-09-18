93ea52140c5b0b7851e0172abe65e087ae0ea4e5
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-18 20:54:31 -0300

    Update UsingScanner.java

--- Java/UsingScanner.java
+++ Java/UsingScanner.java
@@ -1,15 +1,13 @@
-import java.util.*;
 
 public class test {
 	
-	private static Scanner scan;
-
 	public static void main(String[] args) 
 	{
 	
-		scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		System.out.println(a);
+		scan.close();
 		
 	}
 }
