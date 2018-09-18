8cdd2105e9bb862a31e3a0e64a1c7a348bc617af
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-18 20:35:41 -0300

    Create Printing.java

---
+++ Java/Printing.java
@@ -0,0 +1,15 @@
+public class test {
+	
+	public static void main(String[] args) 
+	{
+			
+			int age = 36;
+			String name = "Maria";
+			double height = 1.68564;
+			System.out.printf("%s is %d years old, and is %.2f meters high %n", name, age, height);
+			System.out.println( name + " is " + age + " years old, and is " + height + " meters high");
+			//two different ways to show information
+			//the best way to choose how many decimals in a double/float to use is with the first method
+	
+	}
+}
