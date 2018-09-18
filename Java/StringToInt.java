ceb97f8b54ef11f7b2a52b50db44f97cfdc1922a
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-18 08:35:19 -0300

    Create StringToInt.java

---
+++ Java/StringToInt.java
@@ -0,0 +1,16 @@
+package basicPack;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String s = "33";
+		
+		int a = Integer.parseInt(s);
+		
+		System.out.print(a);
+		
+		//this is how you convert a string into an int for example
+	}
+
+}
