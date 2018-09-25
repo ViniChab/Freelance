565b947424b9c53591b7662689206a23b3f913a8
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-09-25 11:42:26 -0300

    Update CriaConta.java

--- Java/Alura/PacoteConta/src/CriaConta.java
+++ Java/Alura/PacoteConta/src/CriaConta.java
@@ -1,12 +1,24 @@
-
 public class CriaConta {
 
 	public static void main(String[] args) {
 		
 		Conta minhaConta = new Conta(); //a partir de agora, posso  usar Conta como um tipo
 		//funciona pq ele reconhece a classe conta do mesmo pacote
-		minhaConta.saldo = 200;
+		minhaConta.saldo = 300;
+		
+		Conta outraConta = minhaConta; 
+		// duas flechas para a mesma classe, NÃO SÃO DOIS OBJETOS IGUAIS
+
 		System.out.println(minhaConta.saldo);
+		System.out.println(outraConta.saldo); //aqui, temos duas flechas pra messma conta
+		
+		//no fim, temos apenas uma conta
+		
+		//prova =>
+		
+		minhaConta.saldo += 100;
+		
+		System.out.println(outraConta.saldo);
 		
 	}
 
