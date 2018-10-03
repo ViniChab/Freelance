cc62f23cad8c470625baf6e3f6fa513cfb6a95d2
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-10-03 10:30:13 -0300

    Update TryCatch.java

--- Java/Exceptions/TryCatch.java
+++ Java/Exceptions/TryCatch.java
@@ -5,9 +5,12 @@ public static void main(String[] args) {
 			int a = 1/0;
 			System.out.println("It worked! a = " + a);
 		}
-		catch (ArithmeticException err) 
+		catch (ArithmeticException ex) 
 		{
-			System.out.println("ERROR - ArithmeticException");
+			String err = ex.getMessage(); //mensagem (tipo / by zero)
+			System.out.println("ERROR - ArithmeticException " + err);
+			ex.printStackTrace(); /*mostra os métodos que ele passou atravessado 
+			 					   antes de ser resolvido, e onde o erro ocorreu*/
 		}
 		
 		System.out.println("The program keeps running");
