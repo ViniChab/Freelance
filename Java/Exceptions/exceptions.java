fb527eb7a8ba68f30dad286486fd7f8c2fc1a588
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-10-03 11:11:35 -0300

    Update exceptions.java

--- Java/Exceptions/exceptions.java
+++ Java/Exceptions/exceptions.java
@@ -36,4 +36,12 @@ public static void main(String[] args) {
 		b = scan.nextInt();
 		
 		scan.close();
+	
+		/*Exception in thread "main" java.util.InputMismatchException
+		at java.util.Scanner.throwFor(Scanner.java:864)
+		at java.util.Scanner.next(Scanner.java:1485)
+		at java.util.Scanner.nextInt(Scanner.java:2117)
+		at java.util.Scanner.nextInt(Scanner.java:2076)
+		at CriaConta.main(CriaConta.java:35)*/
+	
 	}
