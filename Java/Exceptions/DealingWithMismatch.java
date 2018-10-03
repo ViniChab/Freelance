a2308c0a6c9b129c714d00dd79e182e66e9bce64
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-10-03 11:05:10 -0300

    Update DealingWithMismatch.java

--- Java/Exceptions/DealingWithMismatch.java
+++ Java/Exceptions/DealingWithMismatch.java
@@ -1,25 +1,24 @@
 public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
+	Scanner scan = new Scanner(System.in);
 		
-		int a;
-		boolean workingInput = false;
+	int a;
+	boolean workingInput = false;
 		
-		while( workingInput == false )
-		{
-			try {
-				System.out.print("Digite um valor para a: ");
-				a = scan.nextInt();
-				System.out.println(a);
-				workingInput = true;
-			}catch(Exception err){
-				System.out.println("Por favor, tente novamente");
-				workingInput = false;
-				scan.next();
-			}
-		} 
+	while( workingInput == false )
+	{
+		try {
+			System.out.print("Digite um valor para a: ");
+			a = scan.nextInt();
+			System.out.println(a);
+			workingInput = true;
+		}catch(Exception err){
+			System.out.println("Por favor, tente novamente");
+			workingInput = false;
+			scan.next();
+		}
+	} 
 		
-		scan.close();
-		
-	}
+	scan.close();	
+    }
 }
