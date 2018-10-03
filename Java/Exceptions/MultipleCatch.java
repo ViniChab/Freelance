e19db4ea88405d8d1568992b96e7edeb52a3ea9e
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-10-03 10:46:54 -0300

    Create MultipleCatch.java

---
+++ Java/Exceptions/MultipleCatch.java
@@ -0,0 +1,19 @@
+	public static void main(String[] args) {
+
+		exemplo exe = null;
+
+		
+		try 
+		{
+			exe.ex();
+			int a = 1/0;
+		}
+		catch (NullPointerException | ArithmeticException err ) 
+		{
+			System.out.println("ERROR - " + err.getMessage());
+		}
+		
+		System.out.println("The program keeps running");
+
+	}
+}
