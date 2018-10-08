078e48700a4874fff8f7c8f14565b12530c7c036
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-10-08 11:58:00 -0300

    Add files via upload

---
+++ Java/Alura/ProjetoComPackages/src/br/com/bytebank/Conta/Conta.java
@@ -0,0 +1,96 @@
+package br.com.bytebank.Conta;
+
+import br.com.bytebank.Cliente.Cliente;
+
+public class Conta {
+
+	private String numero;
+	private int agencia;
+	private Cliente titular;
+	private double saldo;
+	
+	public Conta( int agencia, String numero, Cliente titular, double saldo) 
+	{
+		this.numero = numero;
+		this.agencia = agencia;
+		this.saldo = saldo;
+		this.titular = titular;
+	}
+	
+	public void depositar(double valor){
+		
+		if ( this.saldo < 0 ) 
+		{
+			System.out.println("ERRO- O valor deve ser maior que zero");
+			return;
+		}
+		else  this.saldo += valor;
+		
+	}
+	
+	public boolean sacar(double valor) {
+		
+		if ( valor > this.saldo ) 
+		{
+			System.out.println("ERRO- Saldo insuficiente");
+			return false;
+		}
+		else 
+		{
+			this.saldo -= valor;
+			return true;
+		}
+		
+	}
+	
+	public boolean transferir(double valor, Conta destino) {
+		
+		if ( valor > this.saldo ) 
+		{
+			System.out.println("ERRO- Saldo insuficiente");
+			return false;
+		}
+		else
+		{
+			this.saldo -= valor;
+			destino.depositar(valor);
+			return true;
+		}
+	}
+
+	public void extrato(Conta conta) {
+		System.out.println(conta.saldo); 
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getTitular() {
+		return titular.getNome();
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+}
