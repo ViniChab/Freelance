4a3cbdf2021bbe3d1e1695a4f4ecfe08c1d696dc
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-01 22:35:47 -0300

    Update InitialComands.rb

--- Ruby/InitialComands.rb
+++ Ruby/InitialComands.rb
@@ -1,2 +1,16 @@
-print "Hello World" //For printing
-puts "Hello World" //Prints AND breaks line
+print "Hello World" #For printing
+puts "Hello World" #Prints AND breaks line
+  #computes cosine
+  puts Math.cos(0)
+  #computes sine of the number
+  puts Math.sin(0)
+  #computes natural logrithm
+  puts Math.log(1)
+  #computes log of a base b in log(a,b)
+  puts Math.log(100,10)
+  #computes square root of given number
+  puts Math.sqrt(2)
+  #computes tangent of given number
+  puts Math.tan(0)
+#For Maths
+name = gets.chomṕ.to_s #gets your name wit a toString
