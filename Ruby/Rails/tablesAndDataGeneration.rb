c63d5f3b7b5f9248dfb3111f46d3a462aa5c23e8
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-08 15:56:25 -0300

    Update tablesAndDataGeneration.rb

--- Ruby/Rails/tablesAndDataGeneration.rb
+++ Ruby/Rails/tablesAndDataGeneration.rb
@@ -1,2 +1,3 @@
 rails g model Post title:string body:text #This will create a table file in db/migrate, 
 rake db:migrate #for effective table generation
+rake --tasks #For related db tasks rake can do
