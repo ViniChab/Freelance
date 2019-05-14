9b96b6f2aab25eb3e04bf2977668d0e480d84446
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-14 11:08:26 -0300

    Update hero.rb

--- Ruby/tests/lib/hero.rb
+++ Ruby/tests/lib/hero.rb
@@ -1,7 +1,10 @@
+
 class Hero
-    attr_accessor :weapon, :hp
+    attr_accessor :weapon, :hp, :title
     def initialize
         self.weapon = 'Sword'
         self.hp = 2000
+        self.title = 'Hero of Time'
     end
 end
+
