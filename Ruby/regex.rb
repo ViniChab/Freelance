2a5feca0dbe5485890a035fb11181f25bbf93586
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-14 10:00:25 -0300

    Update regex.rb

--- Ruby/regex.rb
+++ Ruby/regex.rb
@@ -10,5 +10,11 @@ puts data #world
 puts data.pre_match #World
 #Ela ainda tem o resto da frase
 
+puts /[1..5]/.match('9,8,7,6,6,7,8,9') #nil, se ele acha ele pr
+
+puts /[a-z]/.match('Oi') #i, é minúsculo
+
+puts /[A-Z]/.match('Oi') #O, é maiúsculo
+
 puts data.post_match #nil
 #Extremely useful
