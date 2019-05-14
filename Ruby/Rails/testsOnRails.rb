98d6b5b21cd48879e1ef53ca5b6c0d4603d0a027
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-14 14:09:31 -0300

    Update testsOnRails.rb

--- Ruby/Rails/testsOnRails.rb
+++ Ruby/Rails/testsOnRails.rb
@@ -2,3 +2,4 @@ bundle exec rspec #actually starting rspec for test
 rails generate model user #Generates both the model and the test files
 rails generate rspec:request User #Generating the test files for a model that already exists
 rails g migration add_info name: string, game: string, numOfAdventures: number #For creating a new migrate file
+rails db:migrate #For updating the db
