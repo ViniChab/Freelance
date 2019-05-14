eb16298d08edb4cd346cc44cdef90becc453e64f
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-14 13:59:37 -0300

    Update testsOnRails.rb

--- Ruby/Rails/testsOnRails.rb
+++ Ruby/Rails/testsOnRails.rb
@@ -1,1 +1,3 @@
 bundle exec rspec #actually starting rspec for test
+rails generate model user #Generates both the model and the test files
+rails generate rspec:request User #Generating the test files for a model that already exists
