87fe4dad823fe37dcb5965594b50a06cb2bafe32
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2019-05-16 11:12:51 -0300

    Add files via upload

---
+++ Ruby/Rails/Projeto/app/controllers/produtos_controller.rb
@@ -0,0 +1,68 @@
+class ProdutosController < ApplicationController
+
+    before_action :set_produto, only: [:edit, :update, :destroy]
+
+    def index
+        @produtos = Produto.order(nome: :asc)
+        @produto_com_desconto = Produto.order(:preco).limit 1
+    end
+    
+    def new
+        @produto = Produto.new
+        @departamentos = Departamento.all
+    end
+    
+    def edit
+        renderiza(:edit) #Só traz na tela
+    end
+
+    def update #Salva o produto editado
+        if @produto.update produto_params
+            flash[:notice] = "Produto atualizado com sucesso!"
+            redirect_to root_url
+        else
+            renderiza(:edit)
+        end
+    end
+    
+    def create
+        @produto = Produto.new produto_params
+        if @produto.save
+            flash[:notice] = "Produto salvo com sucesso!"
+            redirect_to root_url
+        else
+            renderiza(:new)
+        end
+    end
+
+    def destroy
+        @produto.destroy
+        redirect_to root_url
+    end
+    
+    def busca
+        @nome = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome}%"
+    end
+    
+    private
+    
+    def produtos
+        renderiza(:edit) #Só traz na tela
+    end
+
+    def produto_params
+        params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+    end
+
+    def set_produto
+        @produto = Produto.find(params[:id])
+    end
+    
+    def renderiza(pagina)
+        @departamentos = Departamento.all 
+        render pagina
+    end
+    
+    
+end
