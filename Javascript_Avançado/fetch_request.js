ec061394eb41f3ae02c0ca44c79885aed4c1f5e4
Vinícius Chab <36959048+ViniChab@users.noreply.github.com>
2019-12-19 15:37:48 -0300

    Update fetch_request.js

--- Javascript_Avançado/fetch_request.js
+++ Javascript_Avançado/fetch_request.js
@@ -1,1 +1,15 @@
-fetch("http://totvslabs.devfluigidentity.com/api/auth/v1/token", {"credentials":"include","headers":{"accept":"*/*","accept-language":"pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7","cache-control":"no-cache","content-type":"application/x-www-form-urlencoded","pragma":"no-cache"},"referrer":"http://totvslabs.devfluigidentity.com/ui/login?emailAddress=admin@totvslabs.com&password=Totvs@123","referrerPolicy":"no-referrer-when-downgrade","body":"grant_type=password&domain=totvslabs&username=admin%40totvslabs.com&password=Totvs%40123","method":"POST","mode":"cors"}).then( resp => resp.json()).then(data => console.log(data));
+fetch("http://totvslabs.devfluigidentity.com/api/auth/v1/token", {
+  "credentials": "include",
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "no-cache",
+    "content-type": "application/x-www-form-urlencoded",
+    "pragma": "no-cache"
+  },
+  "referrer": "http://totvslabs.devfluigidentity.com/ui/login?emailAddress=admin@totvslabs.com&password=Totvs@123",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": "grant_type=password&domain=totvslabs&username=admin%40totvslabs.com&password=Totvs%40123",
+  "method": "POST",
+  "mode": "cors"
+}).then(resp => resp.json()).then(data => console.log(data));
